9a314e86cf97305e76b3ebd4ebdb22abef734b53
Emmanuel Valentin <ema_110@outlook.es>
2023-06-17 21:15:45 -0600

    Se añadio botón de inicio de sesión

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,10 +1,24 @@
+import Link from 'next/link';
 import { Container, Navbar } from 'react-bootstrap';
+import CustomButton from '../button/button';
 
 export default function CustomNavBar() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
+    <Navbar variant="dark" style={{ backgroundColor: '#A7727D' }}>
       <Container fluid>
-        <Navbar.Brand>Unitrámite</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Brand>
+          <Link
+            href="/"
+            style={{
+              color: '#FFFFFF',
+              textDecoration: 'none',
+            }}
+          >
+            Unitrámite
+          </Link>
+        </Navbar.Brand>
+        <CustomButton type="secondary">Iniciar Sesión</CustomButton>
       </Container>
     </Navbar>
   );
